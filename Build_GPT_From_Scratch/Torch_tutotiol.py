3b2b360dcddda4761b6d71a2e40700b24fd7e12a
kani <kyouko@Kyoukos-MacBook-Air.local>
2025-02-25 10:53:35 +0800

    learning how to build GPT from scratch

---
+++ Build_GPT_From_Scratch/Torch_tutotiol.py
@@ -0,0 +1,8 @@
+'''
+Author: Kyouko
+Date: 2025-02-25 10:51:09
+LastEditTime: 2025-02-25 10:51:55
+Description: 请输入文件描述
+FilePath: /LLM_Learing/Build_GPT_From_Scratch/Torch_tutotiol.py
+'''
+# torch tutoriol ecample code
